e9ffdab3d4d94293d32627785e909bf6169d6f16
Ending2015a <zexlus1126@gapp.nthu.edu.tw>
2020-01-27 21:01:57 +0800

    parallelize Source, add BasePlotObject

--- base.py
+++ base.py
@@ -3,9 +3,17 @@ import os
 import abc
 import sys
 import time
+import inspect
 import logging
 
-from typeing import Any, Union, Tuple
+from typing import Any
+from typing import Union
+from typing import Tuple
+from typing import Callable
+from typing import NoReturn
+from typing import Optional
+from typing import Generator
+from typing import cast
 
 # --- 3rd party ---
 import numpy as np
@@ -13,38 +21,334 @@ import numpy as np
 # --- my module ---
 from cyaegha import logger
 
+from cyaegha.common.draft import is_draft
+from cyaegha.common.draft import Key
+from cyaegha.common.draft import Draft
+from cyaegha.common.draft import Instantiate
+
+from cyaegha.common.forge import argshandler
 from cyaegha.common.route import Route
+
 from cyaegha.common.utils import ParameterPack
 from cyaegha.common.utils import counter
 
+from cyaegha.common.parallel import parallelizable
+from cyaegha.common.parallel import is_parallelizable
+
+from cyaegha.plot.source import Source
+
 __all__ = [
-    'BaseObject',
     'BaseGraph'
 ]
 
+class BasePlotObject(metaclass=abc.ABCMeta):
 
-class BaseObject():
+    _already_setup_object: bool = False
+
+    # draft container
+    _drafts = ParameterPack()
+
+    # meta info
+    # Whether to enable `BasePlotObject._update_step()` for each timestep.
+    step_update: bool = True
+    # Whether to enable `BasePlotObject._update_epoch()` for each epoch.
+    epoch_update: bool = True
+    # Whether to enable `BasePlotObject._update_episode()` for each episode.
+    episode_update: bool = True
+
+    # === Properties ===
+
+    @property
+    def drafts(self) -> ParameterPack:
+        '''
+        Storing `Draftable` objects
+        '''
+        return self._drafts
+
+    @property
+    def outputs(self) -> Any:
+        '''
+        Caching outputs
+        '''
+        return self._cached_outputs
+
+    @property
+    def name(self) -> Optional[str]:
+        '''
+        Return object's name
+        '''
+        return self._name
+
+    # === Main interfaces ===
+
+    def __init__(self, name, log_level='INFO', **kwargs) -> NoReturn:
+        '''
+        Initialize Object
+
+        Args:
+            name: (str or None) object name or logger name
+            log_level: (str) logging level
+        '''
+
+        self._name: Optioanl[str] = name
+
+        # create logger
+        if name is None: name = '<anony>'
+        self.LOG = logger.getLogger(name='{}.{}'.format(type(self).__name__, name), level=log_level)
+
+        # initialize cached outputs
+        self._cached_outputs: Any = None
+
+    def __call__(self, *args, **kwargs) -> Any:
+        '''
+        Main operation
+
+        The results are cached on `outputs` property.
+        '''
+
+        self._cached_outputs = self._forward_object(*args, **kwargs)
+    
+        return self._cached_outputs
+
+    def setup(self, *args, force: bool =True, **kwargs) -> 'BasePlotObject':
+        '''
+        Setup object or sub-modules
+
+        Args:
+            force: (bool) whether to force to setup the object even if it is already set up.
+
+        Returns:
+            self
+        '''
+
+        # check if already set up
+        if (not self._already_setup_object) or (force):
+            # setup objects
+            self._setup_object(force=force, **kwargs)
+            # mark as set up
+            self._already_setup_object = True
+
+        return self
+
+    def update(self, invoke_step: bool =False,
+                     invoke_epoch: bool =False,
+                     invoke_episode: bool =False, **kwargs) -> 'BasePlotObject':
+        '''
+        Update module. 
+        
+        Calling update operations.
+
+        Args:
+            invoke_step: (bool) whether to call `BaseModule._update_step`, if `BaseModule.step_update` is `True`
+            invoke_epoch: (bool) whether to call `BaseModule._update_epoch`, if `BaseModule.epoch_update` is `True`
+            invoke_episode: (bool) whether to call `BaseModule._update_episode`, if `BaseModule.episode_update` is `True`
+
+        Kwargs:
+            timestep: (int) current timesteps
+            epoch: (int) current epoch
+            episode: (int) current episode
+
+        Returns:
+            self
+        '''
+
+        if invoke_step and self.step_update:
+            self._update_step(invoke_step=invoke_step, invoke_epoch=invoke_epoch, invoke_episode=invoke_episode, **kwargs)
+
+        if invoke_epoch and self.epoch_update:
+            self._update_epoch(invoke_step=invoke_step, invoke_epoch=invoke_epoch, invoke_episode=invoke_episode, **kwargs)
+        
+        if invoke_episode and self.episode_update:
+            self._update_episode(invoke_step=invoke_step, invoke_epoch=invoke_epoch, invoke_episode=invoke_episode, **kwargs)
+
+        return self                 
 
 
-class BaseTrace(BaseObject):
+    # === Sub interfaces ===
+
+    @abc.abstractmethod
+    def _setup_object(self, *args, **kwargs):
+        '''
+        Define your setup instructions (setup sub-modules ...etc) for this object.
+        '''
+        pass
+
+    @abc.abstractmethod
+    def _execute_object(self, *args, **kwargs):
+        '''
+        Define the forward pass of this module.
+        '''
+        pass
+
+    def _update_object(self, *args, **kwargs):
+        '''
+        Define the default update instructions of this module
+        '''
+        pass
+
+    def _update_step(self, *args, **kwargs):
+        '''
+        Define the update instructions of this module for the beginning of each timestep. 
+        
+        In default, this function will invoke `BaseModule._setup_module`
+        '''
+        self._update_object(*args, **kwargs)
+
+    def _update_epoch(self, *args, **kwargs):
+        '''
+        Define the update instructions of this module for the beginning of each epoch. 
+        
+        In default, this function will invoke `BaseModule._setup_module`
+        '''
+        self._update_object(*args, **kwargs)
+
+    def _update_episode(self, *args, **kwargs):
+        '''
+        Define the update instructions of this module for the beginning of each episode. 
+        
+        In default, this function will invoke `BaseModule._setup_module`
+        '''
+        self._update_object(*args, **kwargs)
 
 
 
-graph[1, 1].add_trace()
+class BaseTrace(BasePlotObject):
+    '''
+    BaseTrace
+    '''
 
-class BaseSubplotHandler():
-    @classmethod
-    def add_func(cls, args):
+    # === Properties ===
+
+    @property
+    def loaded(self) -> bool:
+        '''
+        Whether the data is loaded
+        '''
+        return (self._already_setup_trace) and (self._source.loaded)
+
+    # === Main interfaces ===
+
+    def __init__(self, name: str, type: Any, source: Source, **kwargs) -> NoReturn:
+        '''
+        Args:
+            type: (Any) trace type
+            source: (Source)
+        '''
+
+        # initialize BasePlotObject
+        super(BaseTrace, self).__init__(name=name, **kwargs)
+
+        assert is_draft(source, Source)
+
+        # draft
+        self.drafts.source = source
+
+        # get trace type
+        self._trace_type = self._get_type(type)
+        self._kwargs = ParameterPack(**kwargs)
+
+        # initialize instances
+        self._source = None
+        self._cached_outputs = None
+        
+    @parallelizable
+    def load(self, input: Any =None, force: bool =False, **kwargs) -> bool:
+        '''
+        Args:
+            input: (Any)
+        Returns:
+            (bool) if is loaded
+        '''
+
+        assert self._already_setup_trace, 'The trace is not ready, please call setup(), first.'
+        
+        if (not self.loaded) or (force):
+            self._loaded_source = self._source(input=input, **kwargs)
+
+        return self.loaded
+
+    # === Sub interfaces ===
+
+    @abc.abstractmethod
+    def _forward_object(self, input: Any =None, combine_source=False, **kwargs) -> Any:
+        '''
+        Args:
+            combine_source: (bool) if the loaded source is a list of pandas.Dataframe, 
+                combine all of them into a single pandas.Dataframe. Otherwise, create
+                a trace for each Dataframe.
+        Returns:
+            (list of traces)
+        '''
+
+        raise NotImplementedError('Method not implemented!')
+
+    def _setup_object(self, key: Key =Draft.default, **kwargs) -> NoReturn:
+
+        # setup source
+        if self._source is None:
+            self._source = Instantiate(self.drafts.source, key=key).setup(key=key, **kwargs)
+    
+
+    @load.unroll
+    def _load_unroll(self, input: Any =None, **kwargs) -> Generator[Callable, None, None]:
+        '''
+        Args:
+            input: (list of Any)
+        Returns:
+            (list of bool) if is loaded
+        '''
+
+        assert self._already_setup_trace, 'The trace is not ready, please call setup(), first.'
+
+        assert is_parallelizable(self._source._forward_module), 'The source must be parallelizable'
+        
+        yield from self._source.unrolled(input=input, **kwargs)
+
+    @load.callback
+    def _load_callback(self, res) -> bool:
+        '''
+        Callback function
+        '''
+        # Source.callback will return True if all of the processes ended
+        if self._source.callback(res):
+            # get outputs
+            self._loaded_source = self._source.outputs
+
+            return True
+
+        return False
+
+    @abc.abstractmethod
+    def _get_type(self, type: Any) -> Any:
+        
+        return type
 
 
-class BaseSubplotHandler():
-    def __init__(self, **kwargs):
+class BaseSubplotHandler(argshandler(sig='self, row, col')):
+    __doc__ = '''\
+    BaseSubplotHandler
 
+    A wrapper to wrap BaseGraph
+    '''
+    def __init__(self, graph: BaseGraph, row: int, col: int, **kwargs) -> NoReturn:
+        super(BaseSubplotHandler, self).__init__(graph, row, col, **kwargs)
+        self.graph = graph
+
+    @property
+    def traces(self):
+        return self.get_traces()
+
+    
+    # served funcs:
+    #     def add_trace(self, trace)
+    #     def get_traces(self)
+    #     def remove_trace(self, trace)
+    
 
 
 class BaseGraph():
     # === built-in methods ===
-    def __init__(self, rows: int=1, cols: int=1, **kwargs):
+    def __init__(self, rows: int=1, cols: int=1, **kwargs) -> NoReturn:
         '''
         Create Graph
 
@@ -55,11 +359,8 @@ class BaseGraph():
         self._subplot = (rows, cols)
         self._kwargs = ParameterPack(**kwargs)
 
-
-        self._subplot = (rows, cols)
-        self._kwargs = ParameterPack(**kwargs)
-
-        self._index_to_rowcol = {}
+        self._subplot_traces = {}
+        self._subplot_indices = {}
 
         # create a counter starting from 0, steping 1
         count = counter()
@@ -69,16 +370,24 @@ class BaseGraph():
             for c in range(self._subplot[1]):
                 if 'specs' in self._kwargs.keys():
                     if self._kwargs.specs[r][c] is not None:
-                        self._index_to_rowcol[count()] = (r, c)
+                        self._subplot_indices[count()] = (r, c)
                 else:
-                    self._index_to_rowcol[count()] = (r, c)
-        
+                    self._subplot_indices[count()] = (r, c)
 
     def __call__(self, *args, **kwargs):
         return self.plot()
 
     def __getitem__(self, key: Union[int, Tuple(int, int)]) -> BaseSubplotHandler:
-        return self.subplot(key)
+        '''
+        Args:
+            key: (int or Tuple(int, int)) index or (row, col)
+        '''
+        if isinstance(key, int):
+            return self.subplot(key)
+        elif isinstance(key, tuple):
+            return self.subplot(*key)
+        else:
+            raise ValueError('Unknown arguments: {}'.format(key))
 
     # === properties ===
     @property
@@ -90,34 +399,89 @@ class BaseGraph():
         return self._subplot[1]
 
     # === functions ===
+
+    @abc.abstractmethod
     def plot(self):
         '''
         Plot graph
         '''
         raise NotImplementedError('Method not implemented')
 
-    @polymethod()
-    def subplot(self, row: int, col: int) -> BaseSubplotHandler:
+
+    def subplot(self, *args) -> BaseSubplotHandler:
+        if len(args) == 1:
+            row, col = self._subplot_indices[args[0]]
+        elif len(args) == 2:
+            row, col = *args
+        else:
+            raise ValueError('Unknown args: {}'.format(args))
+        
         return BaseSubplotHandler(self, row, col)
 
-    @polymethod()
-    def subplot(self, subplot: Tuple[int, int]) -> BaseSubplotHandler:
-        return BaseSubplotHandler(self, subplot[0], subplot[1])
+    @BaseSubplotHandler.serve()
+    def add_trace(self, trace, row=1, col=1):
+        '''
+        Add new trace to subplot
 
-    @polymethod()
-    def subplot(self, index: int) -> BaseSubplotHandler:
-        row, col = self._subplot_index[index]
+        Args:
+            trace: (BaseTrace) new trace to add
+            row: (int) subplot row
+            col: (int) subplot col
+        '''
 
-        return BaseSubplotHandler(self, row, col)
+        # assert (row, col)
+        assert row <= self._subplot[0] and col <= self._subplot[1]
 
-    @polymethod()
-    def subplot(self, *args, **kwargs):
-        raise RuntimeError('subplot only accepts 1 or 2 arguments')
+        # create array
+        if (row, col) not in self._subplot_traces.keys():
+            self._subplot_traces[(row, col)] = []
 
+        self._subplot_traces.append(trace)
 
-    def add_trace(self, trace):
-        #TODO
         return self
 
+    @BaseSubplotHandler.serve()
+    def get_traces(self, row=1, col=1):
+        '''
+        Get traces from subplot
 
-graph[1, 1].add_trace()
+        Args:
+            row: (int) subplot row
+            col: (int) subplot col
+        ''' 
+
+        # assert (row, col)
+        assert row <= self._subplot[0] and col <= self._subplot[1]
+
+        # create array
+        if (row, col) not in self._subplot_traces.keys():
+            self._subplot_traces[(row, col)] = []
+
+        return self._subplot_traces[(row, col)]
+
+    @BaseSubplotHandler.serve()
+    def remove_trace(self, trace, row=1, col=1):
+        '''
+        Remove existing trace
+
+        Args:
+            trace: (BaseTrace or int) the trace object or index in the order of appended to subplot array
+            row: (int) subplot row
+            col: (int) subplot col
+        '''
+
+        assert row <= self._subplot[0] and col <= self._subplot[1]
+
+        if (row, col) not in self._subplot_traces.keys():
+            self._subplot_traces[(row, col)] = []
+
+        if isinstance(trace, int):
+            if trace < len(self._subplot_traces[(row, col)]):
+                del self._subplot_traces[(row, col)][trace]
+
+        elif isinstance(trace, BaseTrace):
+            # remove trace if it contains in the array
+            if trace in self._subplot_traces[(row, col)]:
+                self._subplot_traces[(row, col)].remove(trace)
+            
+        return self
--- source.py
+++ source.py
@@ -3,7 +3,16 @@ import os
 import abc
 import sys 
 import time
+import types
 import logging
+import threading
+
+from typing import Any
+from typing import Union
+from typing import NoReturn
+from typing import Optional
+from typing import Callable
+from typing import Generator
 
 # --- 3rd party ---
 import numpy as np
@@ -13,92 +22,294 @@ import tensorflow as tf
 from cyaegha import logger 
 
 from cyaegha.common.base import BaseModule
+from cyaegha.common.draft import Key
+from cyaegha.common.draft import Draft
+from cyaegha.common.draft import is_draft
 
+from cyaegha.common.parallel import parallelizable
+
+from cyaegha.plot.process import BaseProcess
 
 class Source(BaseModule):
-    def __init__(self, name, src=None, data=None, process=None, static=False, slice_inputs=True, **kwargs):
+
+    # === properties ===
+    @property
+    def static(self) -> bool:
+        '''
+        Whether the source is a static source (only load for once)
+        '''
+        return self._static
+
+    @property
+    def src(self) -> Optional[BaseModule]:
+        '''
+        Source instance
+        '''
+        return self._src
+    
+    @property
+    def process(self) -> Optional[BaseProcess]:
+        '''
+        Process instance
+        '''
+        return self._process
+
+    @property
+    def loaded(self) -> bool:
+        '''
+        Whether the source is loaded
+        '''
+        return self._loaded
+
+    # === Main interfaces ===
+
+    def __init__(self, name: Optional[str], 
+                        src: Optional[BaseModule] =None, 
+                       data: Any =None, 
+                    process: Optional[BaseProcess] =None, 
+                     static: bool =False, 
+               slice_inputs: bool =True, **kwargs) -> NoReturn:
         '''
         Initialize Source
 
         Args:
             name: (str or None)
-            src: (BaseModule) call to get data
-            data: 
-            process: 
-            static: 
-            slice_inputs:
+            src: (BaseModule) callable data
+            data: (list)
+            process: (BaseProcess)
+            static: (bool) Whether this is a static source. If True, the source would be loaded only for once. 
+                However, you can still reload data by assign force=True when calling Source.__call__
+            slice_inputs: (bool) Whether to slice inputs before applying to the process module. BTW, the process
+                of sliced inputs can be accelerated by calling Source.parallelize
+
+        Kwargs:
+            log_level: (str, default: 'INFO') loggin level
         '''
 
         super(Source, self).__init__(name=name, **kwargs)
 
+        if src is not None:
+            assert is_draft(src, BaseModule)
+
+        if process is not None:
+            assert is_draft(process, BaseProcess)
+
         # draft
-        self.draft.src = src
-        self.draft.process = process
+        self.drafts.src = src
+        self.drafts.process = process
         
-        self._data = data
-        self._static = True if static else False
-        self._slice_inputs = slice_inputs
-        self._source_loaded = False
+        # initialize
+        self._data: Any = data
+        self._static: bool = True if static else False
+        self._slice_inputs: bool = True if slice_inputs else False
+        self._loaded: bool = False
         
-        # instances
+        # initialize instances
         self._src = None
         self._process = None
 
-    # === properties ===
-    @property
-    def static(self):
-        return self._static
+    @parallelizable
+    def __call__(self, input: Any =None, force: bool =False, **kwargs):
+        '''
+        Load source
+        '''
 
-    @property
-    def src(self):
-        return self._src
-    
-    @property
-    def process(self):
-        return self._process
+        if (not self.static) or (not self._loaded) or (force):
+            # forward module
+            self._cached_outputs = self._forward_module(input=input, force=force, **kwargs)
+            # mark as loaded
+            self._loaded = True
 
-    # === override BaseModule ===
+        return self._cached_outputs
 
-    def _setup_module(self, **kwargs):
+
+    # === Sub interfaces ===
+
+    def _setup_module(self, key: Key =Draft.default, **kwargs) -> NoReturn:
         '''
         override BaseModule._setup_module
         '''
 
-        if self.draft.src is not None:
-            self._src = Instantiate(self.draft.src).setup(**kwargs)
+        if self.drafts.src is not None:
+            self._src = Instantiate(self.drafts.src, key=key).setup(key=key, **kwargs)
 
-        if self.draft.process is not None:
-            self._process = Instantiate(self.draft.process).setup(**kwargs)
+        if self.drafts.process is not None:
+            self._process = Instantiate(self.drafts.process, key=key).setup(key=key, **kwargs)
 
-
-    def _forward_module(self, input=None, **kwargs):
+    @parallelizable
+    def _forward_module(self, input: Any =None, **kwargs) -> Any:
         '''
         override BaseModule._forward_module
+
+        Input priority: input > src > data
+
+        # TODO support for process-based parallelism
+        Currently only support thread-based prallelism. It is unsafe to use proecssed_based parallelization.
         '''
 
-        if not self._source_loaded:
 
-            # get input from self._src
-            if self._src is not None:
-                input = self._src(input, **kwargs)
-            # get input from self._data
-            elif self._data is not None:
-                input = self._data
+        # Input priority: input > src > data
+        # get input from self._src
+        if self._src is not None:
+            input = input or self._src(input, **kwargs)
 
-            # processing data
-            if self._slice_inputs:
-                output = []
-                for d in input:
-                    output.append(
-                        self._process(d, **kwargs))
+        # get input from self._data
+        if self._data is not None:
+            input = input or self._data
 
-            else:
-                output = self._process(input, **kwargs)       
-
-            # if static, mark as loaded, else not loaded
-            self._source_loaded = self._static
-
+        if self._slice_inputs:
+            output = []
+            for d in input:
+                output.append( self._process(input=d, **kwargs) )
         else:
-            output = self.cached_output
+            output = self._process(input=input, **kwargs)
 
         return output
+
+    @__call__.unroll
+    def _call_unroll(self, input: Any =None, context: Any =None, **kwargs) -> Generator[Callable, None, None]:
+        '''
+        Parallelize __call__
+
+        NOTE: each coroutine returns an object `result`
+
+        Args:
+            input: (Any) input data
+            context: (Any) context objects
+
+        Returns:
+            (Generator)
+        '''
+
+        if (not self.static) or (not self._loaded) or (force):
+
+            yield from self._forward_module.unrolled(input=input, context=context, **kwargs)
+
+        else:
+            # return empty generator (yield nothing)
+            return
+            yield
+
+    @__call__.callback
+    def _call_callback(self, res: Any, context: Any, **kwargs) -> Any:
+        '''
+        Return post-processed result
+
+        Args:
+            res: (Any) result from _forward_module_callback()
+            context: (Any) context object
+
+        Returns:
+            (Any) result
+        '''
+
+        if context.fin:
+            # set outputs
+            self._cached_outputs = context.outputs
+            # set loaded to True
+            self._loaded = True
+        
+        return res
+
+    @_forward_module.unroll
+    def _forward_module_unroll(self, input: Any =None, context: Any=None, **kwargs) -> Generator[Callable, None, None]:
+        '''
+        Parallelize _forward_module
+
+        NOTE: each coroutine returns (index, result) tuple
+
+        Args:
+            input: (Any) input data
+            context: (Any) context objects
+
+        Returns:
+            (Generator)
+        '''
+
+        # Input priority: input > src > data
+        # get input from self._src
+        if self._src is not None:
+            input = input or self._src(input, **kwargs)
+
+        # get input from self._data
+        if self._data is not None:
+            input = input or self._data
+
+        # initialize context
+        context.sliced = self._slice_inputs
+        context.total_completed = 0
+        context.outputs = []
+        context.fin = False
+
+        if self._slice_inputs:
+            
+            # initialize context
+            context.total_coroutines = len(input)
+            
+            # create coroutines for each slice
+            for idx, d in enumerate(input):
+                def _coroutine_factory(index, process, input, kwargs):
+
+                    def _coroutine(context):
+                        result = process(input, **kwargs)
+                        return (index, result)
+
+                    return _coroutine
+
+                yield _coroutine_factory(idx, self._process, d, kwargs)
+
+        else:
+
+            # initialize context
+            context.total_coroutines = 1
+                
+            def _coroutine_factory(index, process, input, kwargs):
+
+                def _coroutine():
+                    result = process(input, **kwargs)
+                    return (index, result)
+
+                return _coroutine
+                
+            yield _coroutine_factory(0, self._process, d, kwargs)
+
+
+    @_forward_module.callback # thread safe
+    def _forward_module_callback(self, res: tuple, context: Any, **kwargs) -> Any:
+        '''
+        Return post-processed result
+
+        Args:
+            res: (tuple) a tuple of (index, result)
+            context: (Any) context objects
+        
+        Returns:
+            (Any) result
+        '''
+
+        # append results
+        context.temp_outputs.append(res)
+        # increase counter
+        context.total_completed += 1
+
+        # finalize
+        context.fin = (context.total_coroutines == context.total_completed)
+        if context.fin:
+
+            # only one output
+            if not context.sliced:
+                # only one output
+                context.outputs = context.temp_outputs[0]
+            else:
+                # multiple outputs, sorted outputs
+                context.outputs = [x[1] for x in sorted(context.temp_outputs, key=lambda x: x[0])]
+
+        return res[1]
+
+
+    # === alias ===
+    def parallelize(self, *args, **kwargs):
+        return self.__call__.parallelize(*args, **kwargs)
+
+    def unrolled(self, *args, **kwargs):
+        return self.__call__.unrolled(*args, **kwargs)
