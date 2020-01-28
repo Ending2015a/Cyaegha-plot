76b19a026b116734f6a510a3d0a9f1852633bd3a
Ending2015a <zexlus1126@gapp.nthu.edu.tw>
2020-01-28 20:45:29 +0800

    update code

--- _plotly/graph.py
+++ _plotly/graph.py
@@ -1,14 +1,27 @@
 # --- built in ---
 import os
+import abc
 import sys
 import time
 import logging
 
+from typing import Any
+from typing import Union
+from typing import List
+from typing import Tuple
+from typing import Callable
+from typing import NoReturn
+from typing import Optional
+from typing import Hashable
+
 # --- 3rd party ---
 import numpy as np
+import pandas as pd
+import scipy.stats as st
 
-import ployly
+import plotly
 import plotly.graph_objects as go
-import plotly.subplots import make_subplots
+
+from plotly.subplots import make_subplots
 
 # --- my module ---
